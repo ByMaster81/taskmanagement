3fa90569e5392d81af5f3ef88b53060da939e8b0
Rüştü Yemenici <rustunaz@hotmail.com>
2025-09-08 17:09:47 +0300

    CRUD

--- routes/assignments.js
+++ routes/assignments.js
@@ -4,12 +4,35 @@ import { PrismaClient } from '@prisma/client';
 const router = Router();
 const prisma = new PrismaClient();
 
-// GET /assignments
+//Read
 router.get('/', async (req, res) => {
   const assignments = await prisma.assignment.findMany({
     include: { user: true, task: true },
   });
   res.json(assignments);
 });
+
+//Create
+router.post('/', async(req,res) => {
+    const { userId, taskId } = req.body;
+    const assignment = await prisma.assignment.create({data: { userId, taskId}});
+    res.json(assignment);
+});
+
+//Update
+router.put('/:id', async(req,res) => {
+    const {id} = req.params;
+    const {userId, taskId} = req.body;
+    const assignment = await prisma.assignment.update({where:{ id }, data:{ userId, taskId}});
+    res.json(assignment);
+});
+
+//Delete
+router.delete('/:id', async(req, res) => {
+    const{id} = req.params;
+    await prisma.assignment.delete({where: {id}});
+    res.json({message: 'Assignment Deleted'});
+});
+
 
 export default router;
--- routes/tasks.js
+++ routes/tasks.js
@@ -12,11 +12,27 @@ router.get('/', async (req, res) => {
   res.json(tasks);
 });
 
-//Add
+//Create
 router.post('/', async (req, res) => {
   const {title, description} = req.body;
   const task = await prisma.task.create({ data: {title, description}});
   res.json(task);
 });
+
+//Update
+router.put('/:id', async(res, req) => {
+  const {id} = req.params;
+  const{title, description} = req.body;
+  const task = await prisma.user.update({where: {id}, data:{title, description}});
+  res.json(task);
+});
+
+
+//Delete 
+router.delete ('/:id', async(req, res) => {
+  const {id} = req.params;
+  await prisma.task.delete({where:{id}});
+})
+
 
 export default router;
