dba89940e18259376099ec89be4a2a9e13a8b8f7
Rüştü Yemenici <rustunaz@hotmail.com>
2025-11-05 21:02:34 +0300

    Güvenlik geliştirmesi

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,28 +1,34 @@
-#!/bin/sh 
-
-
-# Bu script'in amacı, seed işleminin sadece container ilk defa oluşturulduğunda
-# bir kez çalışmasını sağlamaktır.
-
-
+#!/bin/sh
 set -e
-INIT_FLAG_FILE="/app/db_initialized.flag"
 
+INIT_FLAG_FILE="/app/data/db_initialized.flag"
+
+echo "Waiting for database..."
+until pg_isready -h db -p 5432 -U "${POSTGRES_USER}"; do
+  echo "Database is unavailable - sleeping"
+  sleep 1
+done
+
+echo "Database is ready!"
 echo "Running database migrations..."
 npx prisma migrate deploy
 
+# Advisory lock
+echo "Acquiring advisory lock to prevent race conditions..."
+psql "$DATABASE_URL" -c "SELECT pg_advisory_lock(123456);"
 
 if [ ! -f "$INIT_FLAG_FILE" ]; then
-    
     echo "Database not seeded yet. Running seed script..."
     npx prisma db seed
+    mkdir -p /app/data
     touch "$INIT_FLAG_FILE"
-    echo "Seeding complete. Initialization flag created."
+    echo "Seeding complete."
 else
-    echo "Database already seeded. Skipping seed script."
+    echo "Database already seeded. Skipping."
 fi
+
+# Advisory unlock
+psql "$DATABASE_URL" -c "SELECT pg_advisory_unlock(123456);"
 
 echo "Starting application..."
 exec "$@"
-
-  
